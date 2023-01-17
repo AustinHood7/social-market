58b424a18d716eb875ab3f2fce09be29b8b55d78
Austin Hood <austinmhood@live.com>
2023-01-16 20:00:49 -0500

    fixed some of the sections

--- components/BenefitCard.jsx
+++ components/BenefitCard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function BenefitCard(props) {
   return (
     <div className='md:max-w-[25vw] border rounded-xl bg-gradient-to-r from-sky-600 to-emerald-600 flex flex-col p-5 px-10 items-center mx-8'>
-      <h2 className='mb-2 text-4xl text-sky-100'>{props.title}</h2>
+      <h2 className='mb-2 text-4xl text-sky-100 flex justify-center'>{props.title}</h2>
         <Image
             src={props.src}
             alt=''
