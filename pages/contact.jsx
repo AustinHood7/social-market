ff842e48a8fcdd56de75eaeca4599c438d83b3fb
austin.hood <Austinmhood@live.com>
2023-07-13 15:32:58 -0400

    Made changes to contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import LandingPage2 from '../components/LandingPage2'
 import Navbar from '../components/Navbar'
 import { MdEmail } from 'react-icons/md'
+import { BsPersonCircle} from 'react-icons/bs'
+import { FaPhone } from 'react-icons/fa'
 import PageFooter from '../components/PageFooter'
 import { Container } from 'postcss'
 
@@ -14,13 +16,23 @@ function contact() {
         info="Contact"  
       />
       <div className='relative z-20 flex justify-center pt-[5vh] bg-white text-sky-900'>
-        <div className='flex flex-col gap-10'>
-          <h1 className='text-2xl'>Linkedin:</h1>
-          <a className='text-xl' href="https://www.linkedin.com/in/austin-hood7/">https://www.linkedin.com/in/austin-hood7/</a>
-          <h1 className='text-2xl'>Github:</h1>
-          <a className='text-xl' href="https://www.linkedin.com/in/austin-hood7/">https://www.linkedin.com/in/austin-hood7/</a>
+        <div className='flex flex-col gap-6'>
+          <h1 className='text-2xl'>Creator:</h1>
+            <div className='flex items-center gap-6'>
+              <BsPersonCircle color='sky-900' size={45}/>
+              <a className='text-xl'>Parker Joyce</a>
+            </div>
+          <h1 className='text-2xl'>Phone:</h1>
+          <div className='flex items-center gap-6'>
+              <FaPhone color='sky-900' size={45}/>
+              <a className='text-xl'>(239) 284-9089</a>
+            </div>
+          
           <h1 className='text-2xl'>Email</h1>
-          <a className='pb-10' href="mailto:parkerjoyce@soleindustries.com"><MdEmail color='sky-900' size={45}/></a>
+          <div className='flex text-xl items-center gap-6 pb-10'>
+            <a className='' href="mailto:parkerjoyce@soleindustries.com"><MdEmail color='sky-900' size={45}/></a>
+            <span className=''>ParkerJoyce@SoleIndustries.com</span>
+          </div>
         </div>
       </div>
       <PageFooter />
