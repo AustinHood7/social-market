7d1f0c7881280e818b86f33f5f90bbb38d018366
austin.hood <Austinmhood@live.com>
2023-07-13 15:38:21 -0400

    fixed spacing bug for prod

--- components/GraphBox1.jsx
+++ components/GraphBox1.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 function GraphBox1(props) {
   return (
-    <div className='md:w-[18vw] h-[15   rem] bg-gradient-to-r from-sky-600 to-emerald-600 border rounded-2xl bg-opacity-40 
+    //fixed bug here
+    <div className='md:w-[18vw] h-[15.1rem] bg-gradient-to-r from-sky-600 to-emerald-600 border rounded-2xl bg-opacity-40 
     text-sky-100 font-bold text-4xl flex items-center justify-center p-[3%] px-5'>
       {props.sign_up}
     </div>
